70ebbfbd20cc42bf1e1765a7f851de459a86f47d
Orlando Banunaek <johannesbanunaek@gmail.com>
2025-06-12 18:10:50 +0000

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/UAS_PTI_GAME_JMK48",
   server: {
     allowedHosts: ["cf8w8r-5173.csb.app"],
     allowedHosts: ["cf8w8r-5175.csb.app"],
